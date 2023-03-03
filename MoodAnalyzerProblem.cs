5e013022c044d92a6cbc04559c07cf55e552c043
poojakhandale <poojakhandale2000@gmail.com>
2023-03-03 23:22:31 +0530

    respond happy or sad mood massage

---
+++ MoodAnalyzerProblem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+   public  class MoodAnalyzerProblem
+    {
+        public string Mood(string m)
+        {
+            if (m == "Happy")
+            {
+                return "HAPPY";
+            }
+            else
+            {
+                return "SAD";
+            }
+        }
+    }
+}
