b67a65187c909face5ba1053bbfb605da0ffb516
poojakhandale <poojakhandale2000@gmail.com>
2023-03-05 19:13:59 +0530

    Mood analyzer program

--- MoodAnalyzerProblem.cs
+++ MoodAnalyzerProblem.cs
@@ -6,18 +6,20 @@ using System.Threading.Tasks;
 
 namespace MoodAnalyzer
 {
-   public  class MoodAnalyzerProblem
+    public class MoodAnalyser
     {
-        public string Mood(string m)
+        public string n;
+        public MoodAnalyser(string name)
         {
-            if (m == "Happy")
-            {
-                return "HAPPY";
-            }
-            else
-            {
-                return "SAD";
-            }
+            this.n = name;
+            Console.WriteLine(n);
         }
+
+        public string mood()
+        {
+            return this.n;
+        }
+
     }
-}
+    }
+
