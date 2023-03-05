6ce4af360a12d6d27d8e00145a738c0a7d43648f
poojakhandale <poojakhandale2000@gmail.com>
2023-03-05 19:15:06 +0530

    Handle exception for invalid

--- Program.cs
+++ Program.cs
@@ -1,8 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 public class Program
 {
-    public static void Main(string[] args)
-    {
-       
+   
+
+        public static void Main(string[] args)
+        {
+            Program.ErrorFun();
+        }
+
+        public static void ErrorFun()
+        {
+
+            Console.WriteLine("Enter the Mood Happy or Sad: ");
+            string mood = Console.ReadLine();
+            try
+            {
+                if (mood == "Happy")
+                {
+                    Console.WriteLine("Happy");
+                }
+                if (mood == "Sad")
+                {
+                    Console.WriteLine("Sad");
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
     }
-}
+
+
+       
+    
+
