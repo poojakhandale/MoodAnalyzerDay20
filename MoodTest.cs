c04e0146d02bbfdb88995f50713ca59b8d73c530
poojakhandale <poojakhandale2000@gmail.com>
2023-03-05 18:21:06 +0530

    Test case 1.2

---
+++ MoodTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using   MoodAnalyzer;
+
+namespace MoodTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Mood_Success()
+        {
+            //Arrange
+            string h = "Sad";
+            MoodAnalyzer m1 = new MoodAnalyzer();
+            string expectedOutput = "SAD";
+
+            //Act 
+            string actualOutput = m1.Mood(h);
+
+            //Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
+
+
