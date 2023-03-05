11a1bac7ef39be465908fa6e6cfb9069177c8251
poojakhandale <poojakhandale2000@gmail.com>
2023-03-05 19:33:29 +0530

    created custom exception to handle null massage

---
+++ CheckMassageException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class CheckMassageException : Exception
+    {
+        public CheckMassageException(string message) : base(message)
+        {
+
+            
+            
+        }
+    }
+}
